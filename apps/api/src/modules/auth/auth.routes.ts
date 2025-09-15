9084cb91bdd85ea3ab302a34ebf2b749dba35c46
Zhivko Shopov <zhishopov@gmail.com>
2025-09-15 12:10:23 +0100

    Add login service

--- apps/api/src/modules/auth/auth.routes.ts
+++ apps/api/src/modules/auth/auth.routes.ts
@@ -1,5 +1,3 @@
 import { Router } from "express";
 
 const router = Router();
-
-// router.post("signup", signup);
--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -12,7 +12,7 @@ export async function signup(input: {
   password: string;
   role: Role;
 }) {
-  const email = input.email.toLowerCase();
+  const email = input.email.toLowerCase().trim();
   const hash = await bcrypt.hash(input.password, 10);
 
   // Insert user into db
@@ -24,4 +24,41 @@ export async function signup(input: {
   );
 
   return result.rows[0];
+}
+
+export async function login(input: { email: string; password: string }) {
+  const email = input.email.toLowerCase().trim();
+
+  const result = await pool.query(
+    `SELECT id, email, role, password_hash FROM users WHERE email=$1`,
+    [email]
+  );
+
+  const user = result.rows[0];
+  if (!user) {
+    // attach status so the middleware doesn't always default to 500 - server error (this is a client error)
+    throw Object.assign(new Error("Invalid credentials"), { status: 400 });
+  }
+
+  const isValidPassword = await bcrypt.compare(
+    input.password,
+    user.password_hash
+  );
+
+  if (!isValidPassword) {
+    // client error so we attach a status
+    throw Object.assign(new Error("Invalid credentials"), { status: 400 });
+  }
+
+  const token = jwt.sign(
+    {
+      sub: String(user.id),
+      email: user.email,
+      role: user.role as Role,
+    },
+    JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  return token;
 }
